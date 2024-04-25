b5769f09faf0e94625c4b60a9a82514b7904ab84
Mato <msmojvir1@gmail.com>
2024-04-25 21:08:20 +0200

    added a few animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
 
+const arrow = {
+  rest: { rotate: 0 },
+  hover: { rotate: 360 },
+};
 
-  const App = () => {
-    const [cube, setCube] = useState({ x: "45%", y: "40%" });
-    const handleCubeClick = () => {
-      const X = `${Math.random() * 100}%`;
-      const Y = `${Math.random() * 100}%`;
-      setCube({ x: X, y: Y });
-    };
-    return (
-      <>
-        <motion.div
-          onClick={handleCubeClick}
-          className="cube"
-          animate={{position:"absolute", left: cube.x, top: cube.y }}
-          whileHover={{ scale: 1.2 }}
-          whileTap={{ scale: 1.1 }}
-        />
-      </>
-    );
+const App = () => {
+  const [cube, setCube] = useState({ x: "45%", y: "40%" });
+
+  const handleCubeClick = () => {
+    const X = `${Math.random() * 100}%`;
+    const Y = `${Math.random() * 100}%`;
+    setCube({ x: X, y: Y });
   };
+  return (
+    <>
+      <motion.div
+        transition={{ type: "spring", stiffness: 10, damping: 10, mass: 10 }}
+        onClick={handleCubeClick}
+        className="cube"
+        variants={arrow}
+        initial="rest"
+        whileHover="hover"
+        animate={{ position: "absolute", left: cube.x, top: cube.y }}
+        whileTap={{ scale: 1.1 }}
+      />
+    </>
+  );
+};
 
-  export default App;
+export default App;
